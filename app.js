296e995f109b32f04c7e6149bf1109fb65d19e1c
Benjamin Peters <Benjamin.peters@gmail.com>
2018-03-03 02:06:51 -0500

    Added how to play

--- app.js
+++ app.js
@@ -47,10 +47,16 @@ const displayInfo = () => {
     const $infobar = $('<div>').addClass('info');
     const $name = $('<h3>');
     const $health = $('<p>');
+    const $controls = $('<h3>');
+    const $attackbtn = $('<p>');
+    const $defendbtn = $('<p>');
     $name.text(player.name);
     $health.text('Health: ' + player.health);
+    $controls.text('How to play:');
+    $attackbtn.text('Press A: (Attack) cast fireball');
+    $defendbtn.text('Press D: (Defend) cast fire shield');
     $infobar.append($name, $health);
-    $('body').prepend($infobar);
+    $('body').prepend($infobar, $controls, $attackbtn, $defendbtn);
 }
 
 //move and attack with character
@@ -150,26 +156,26 @@ const generateEnemy = (speed) => {
   $('#game-board').append($enemy);
 }
 
-function collision($div1, $div2) {
-    var x1 = $div1.offset().left;
-    var y1 = $div1.offset().top;
-    var h1 = $div1.outerHeight(true);
-    var w1 = $div1.outerWidth(true);
-    var b1 = y1 + h1;
-    var r1 = x1 + w1;
-    var x2 = $div2.offset().left;
-    var y2 = $div2.offset().top;
-    var h2 = $div2.outerHeight(true);
-    var w2 = $div2.outerWidth(true);
-    var b2 = y2 + h2;
-    var r2 = x2 + w2;
-
-    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2){
-     console.log('collision');
-    }else{
-     console.log('hmm');
-    }
-}
+// function collision($div1, $div2) {
+//     var x1 = $div1.offset().left;
+//     var y1 = $div1.offset().top;
+//     var h1 = $div1.outerHeight(true);
+//     var w1 = $div1.outerWidth(true);
+//     var b1 = y1 + h1;
+//     var r1 = x1 + w1;
+//     var x2 = $div2.offset().left;
+//     var y2 = $div2.offset().top;
+//     var h2 = $div2.outerHeight(true);
+//     var w2 = $div2.outerWidth(true);
+//     var b2 = y2 + h2;
+//     var r2 = x2 + w2;
+//
+//     if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2){
+//      console.log('collision');
+//     }else{
+//      console.log('hmm');
+//     }
+// }
 
 $(() => {
     startGame();
