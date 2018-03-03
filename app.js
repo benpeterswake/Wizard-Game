af979e7a05ec171606150755f1448482904bce71
Benjamin Peters <Benjamin.peters@gmail.com>
2018-03-03 01:13:18 -0500

    Added enemy and info bar

--- app.js
+++ app.js
@@ -4,41 +4,64 @@ class Hero {
     this.health = 100;
   }
 }
+class Enemy {
+  constructor(){
+    this.name = 'Skeleton';
+    this.health = 100;
+  }
+}
 
+//Create hero object
+const player = new Hero();
+
+//Start game
 const startGame = () => {
-  startCompleted = true;
   $('#createPlayer').show('slow');
   $('#hero').hide();
+  $('#game-board').hide();
   $('#button').on('click', () => {
-    let name = $('#name').val()
-    const player = new Hero(name);
-    console.log(player);
-
+    //get username
+    player.name = $('#name').val()
     //Hide name-input and show player
     $('#createPlayer').hide();
+    $('#game-board').show()
     $('#hero').show('slow');
     //Show opening text
     const $div = $('<div>').addClass('start').text('You awake in a field...');
     $div.show('slow');
     setTimeout(() => {
-      $div.text('..with no memory of how you go there.');
+      $div.text('..with no memory of how you got there.');
     },2500);
     setTimeout(() => {
       $div.hide('slow');
+      displayInfo();
+      generateEnemy(15000);
+      generateEnemy(12000);
     },7000);
     $('body').prepend($div);
   });
 }
 
+//display the user info bar at top of screen
+const displayInfo = () => {
+    const $infobar = $('<div>').addClass('info');
+    const $name = $('<h3>');
+    const $health = $('<p>');
+    $name.text(player.name);
+    $health.text('Health: ' + player.health);
+    $infobar.append($name, $health);
+    $('body').prepend($infobar);
+}
+
+//move and attack with character
 const moveCharacter = () => {
   $(document).keydown(function(event){
-
     let $hero = $('#hero');
     let $board = $('#game-board');
     if(($hero.css('left') > '0px') && ($hero.css('left') !==  (($( window ).width()-100)+'px'))){
       //hero1
       if(event.which == "37"){
-        $hero.css('transform', 'scaleX(1)');
+        $hero.css('transform', 'scaleX(-1)');
         $hero.animate({left: "-=8"}, 0);
       }
       if(event.which == "39") {
@@ -53,7 +76,7 @@ const moveCharacter = () => {
         $hero.prepend($defend);
         setTimeout(() => {
             $('.defend').remove();
-        }, 4000)
+        }, 4000);
       }
       if(event.which == "65"){
         let width = ("+=" + $( window ).width() + 'px');
@@ -66,12 +89,12 @@ const moveCharacter = () => {
         $attack.css('margin-left', location);
         $(".attack").animate({
         marginLeft: width,
-      }, 1500);
+        }, 1500);
       }
     }else if($hero.css('left') < '0px'){
       //hero1
       if(event.which == "37"){
-        $hero.css('transform', 'scaleX(1)');
+        $hero.css('transform', 'scaleX(-1)');
         $hero.animate({left: "-=0"}, 0);
       }
       if(event.which == "39") {
@@ -91,7 +114,7 @@ const moveCharacter = () => {
     }else if($hero.css('left') == (($( window ).width()-100)+'px')){
       //hero1
       if(event.which == "37"){
-        $hero.css('transform', 'scaleX(1)');
+        $hero.css('transform', 'scaleX(-1)');
         $hero.animate({left: "-=8"}, 0);
       }
       if(event.which == "39") {
@@ -111,9 +134,43 @@ const moveCharacter = () => {
     }
   });
 }
-$(() => {
-  let startCompleted = Boolean;
-  startGame();
-  moveCharacter();
 
+const generateEnemy = (speed) => {
+  const skully = new Enemy();
+  let start = ($( window ).width() + 'px');
+  let end = ("-=" + $( window ).width() + 'px');
+  const $enemy = $('<div>').addClass('enemy');
+  const $img = $('<img>').attr('src','images/enemy.png').addClass('enemyImg');
+  $enemy.append($img);
+  $enemy.css('margin-left', start);
+  $enemy.animate({
+  marginLeft: end,
+  }, speed);
+  $('#game-board').append($enemy);
+}
+
+function collision($div1, $div2) {
+    var x1 = $div1.offset().left;
+    var y1 = $div1.offset().top;
+    var h1 = $div1.outerHeight(true);
+    var w1 = $div1.outerWidth(true);
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+    var x2 = $div2.offset().left;
+    var y2 = $div2.offset().top;
+    var h2 = $div2.outerHeight(true);
+    var w2 = $div2.outerWidth(true);
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
+
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2){
+     console.log('collision');
+    }else{
+     console.log('hmm');
+    }
+}
+
+$(() => {
+    startGame();
+    moveCharacter();
 });
