a3fc94e0a0ce23409b97d02d3ce34d14908b6dae
Benjamin Peters <Benjamin.peters@gmail.com>
2018-03-03 15:29:49 -0500

    first battle added and ability to shoot and kill enemies

--- app.js
+++ app.js
@@ -4,28 +4,57 @@ class Hero {
     this.health = 100;
   }
 }
+
 class Enemy {
+
   constructor(){
     this.name = 'Skeleton';
     this.health = 100;
+    this.enemy = $('<div>').addClass('enemy');
+    this.img = $('<img>').attr('src','images/enemy.png').addClass('enemyImg');
+  }
+  generateEnemy(speed){
+    let start = ($( window ).width() + 'px');
+    let end = ("-=" + $( window ).width() + 'px');
+    this.enemy.append(this.img);
+    this.enemy.css('margin-left', start);
+    this.enemy.animate({
+    marginLeft: end,
+    }, speed);
+    $('#game-board').append(this.enemy);
+  }
+
+  hit(){
+    this.health -= 20;
+    if(this.health<=0){
+      this.img.remove();
+    }else{
+      console.log('still alive');
+    }
   }
 }
 
-//Create hero object
+//Create hero object and enemy
 const player = new Hero();
-
+const skully = new Enemy();
+const skully2 = new Enemy();
 //Start game
 const startGame = () => {
   $('#createPlayer').show('slow');
   $('#hero').hide();
+  $('#mark').hide();
   $('#game-board').hide();
   $('#button').on('click', () => {
     //get username
     player.name = $('#name').val()
     //Hide name-input and show player
     $('#createPlayer').hide();
-    $('#game-board').show()
-    $('#hero').show('slow');
+    $('#game-board').show();
+    setTimeout(()=>{
+      $('#mark').show();
+      $('#hero').show('slow');
+    }, 4000)
+
     //Show opening text
     const $div = $('<div>').addClass('start').text('You awake in a field...');
     $div.show('slow');
@@ -34,12 +63,16 @@ const startGame = () => {
     },2500);
     setTimeout(() => {
       $div.hide('slow');
+      $('#mark').hide('slow');
       displayInfo();
-      generateEnemy(15000);
-      generateEnemy(12000);
     },7000);
     $('body').prepend($div);
   });
+}
+
+const battle1 = () => {
+  skully.generateEnemy(15000);
+  skully2.generateEnemy(17000);
 }
 
 //display the user info bar at top of screen
@@ -54,10 +87,11 @@ const displayInfo = () => {
     $health.text('Health: ' + player.health);
     $controls.text('How to play:');
     $attackbtn.text('Press A: (Attack) cast fireball');
-    $defendbtn.text('Press D: (Defend) cast fire shield');
+    $defendbtn.text('Press Arrows: movement');
     $infobar.append($name, $health);
     $('body').prepend($infobar, $controls, $attackbtn, $defendbtn);
 }
+
 
 //move and attack with character
 const moveCharacter = () => {
@@ -84,6 +118,7 @@ const moveCharacter = () => {
             $('.defend').remove();
         }, 4000);
       }
+      //Attack with fireball
       if(event.which == "65"){
         $hero.css('transform', 'scaleX(-1)');
         let width = ("+=" + $( window ).width() + 'px');
@@ -91,12 +126,35 @@ const moveCharacter = () => {
         $('#game-board .attack').eq(10).remove();
         const $attack = $('<div>').addClass('attack');
         const $img = $('<img>').attr('src','images/fire.png').addClass('fireball');
-        $attack.append($img);
-        $board.prepend($attack);
-        $attack.css('margin-left', location);
-        $(".attack").animate({
-        marginLeft: width,
-        }, 1500);
+          $attack.append($img);
+          $board.prepend($attack);
+          $attack.css('margin-left', location);
+          $(".attack").animate({
+              left: width,
+          }, 1500);
+        setInterval(function collision() {
+          var delta = ($attack.width() + $(".enemyImg").width()) * 0.5 ;
+          var x1 = $attack.offset().left;
+          var y1 = $attack.offset().top;
+          var x2 = $(".enemyImg").offset().left;
+          var y2 = $(".enemyImg").offset().top;
+          if (
+               x1 >= x2 - delta &&
+               x1 <= x2 + delta &&
+               y1 >= y2 - delta &&
+               y1 <= y2 + delta
+           ) {
+              $attack.hide();
+              skully.hit();
+              if(skully.health <=0 ){
+                console.log(skully.health);
+                skully2.hit();
+              }
+           } else {
+              clearInterval(collision);
+           }
+        }, 10);
+
       }
     }else if($hero.css('left') < '0px'){
       //hero1
@@ -142,42 +200,8 @@ const moveCharacter = () => {
   });
 }
 
-const generateEnemy = (speed) => {
-  const skully = new Enemy();
-  let start = ($( window ).width() + 'px');
-  let end = ("-=" + $( window ).width() + 'px');
-  const $enemy = $('<div>').addClass('enemy');
-  const $img = $('<img>').attr('src','images/enemy.png').addClass('enemyImg');
-  $enemy.append($img);
-  $enemy.css('margin-left', start);
-  $enemy.animate({
-  marginLeft: end,
-  }, speed);
-  $('#game-board').append($enemy);
-}
-
-// function collision($div1, $div2) {
-//     var x1 = $div1.offset().left;
-//     var y1 = $div1.offset().top;
-//     var h1 = $div1.outerHeight(true);
-//     var w1 = $div1.outerWidth(true);
-//     var b1 = y1 + h1;
-//     var r1 = x1 + w1;
-//     var x2 = $div2.offset().left;
-//     var y2 = $div2.offset().top;
-//     var h2 = $div2.outerHeight(true);
-//     var w2 = $div2.outerWidth(true);
-//     var b2 = y2 + h2;
-//     var r2 = x2 + w2;
-//
-//     if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2){
-//      console.log('collision');
-//     }else{
-//      console.log('hmm');
-//     }
-// }
-
 $(() => {
     startGame();
     moveCharacter();
+
 });
